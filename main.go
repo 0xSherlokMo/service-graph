b386547cff991fff882627fa622492729d668ce4
0xSherlokMo <mohamed.sorour@halan.com>
2023-03-24 00:17:27 +0200

    entrypoint: added entrypoint to the service

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"log"
+	"net"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/graduation-fci/service-graph/dependencies"
+	"github.com/graduation-fci/service-graph/proto"
+	"github.com/graduation-fci/service-graph/server"
+	"google.golang.org/grpc"
 )
 
 func main() {
+	dp := dependencies.NewDependencyInjection().WithNeo4j()
+	defer dp.Shutdown()
+
 	go healthCheck()
+	StartGRPC(dp)
+}
+
+func StartGRPC(dp *dependencies.DP) {
+	listner, err := net.Listen("tcp", ":"+os.Getenv("GRPC_PORT"))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	graphServer := server.NewGraphServer(dp)
+	proto.RegisterGraphServiceServer(grpcServer, graphServer)
+	log.Printf("server listening at %v", listner.Addr())
+	if err := grpcServer.Serve(listner); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
 
 func healthCheck() {
