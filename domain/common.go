9a8c897b54aaa0529d52d92070074a3d496f0552
0xSherlokMo <mohamed.sorour@halan.com>
2023-03-24 20:36:03 +0200

    model: common closure function

--- domain/common.go
+++ domain/common.go
@@ -1,3 +1,25 @@
 package domain
 
+import (
+	"context"
+	"log"
+)
+
 type Empty = struct{}
+
+type resource string
+
+const ResourceNeo4j = "[Neo4j]"
+
+type ResourceWithClosure interface {
+	Close(ctx context.Context) error
+}
+
+type CloserFunc = func()
+
+func HandleClosure(ctx context.Context, connection ResourceWithClosure, tag resource) {
+	err := connection.Close(ctx)
+	if err != nil {
+		log.Println(tag, "Error while closing resource connection")
+	}
+}
